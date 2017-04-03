3c281d123270623aaa52550185183c97732e9ef4
Erik Platt <eplatt510@gmail.com>
2017-04-03 20:03:33 +0000

    edit app.js

--- app.js
+++ app.js
@@ -3,16 +3,6 @@ var app = express();
 
 app.use("/", express.static(__dirname + '/views'));
 
-// app.use(express.static('views'));
-// app.use(express.static('css'));
-// app.use(express.static('js'));
-
-// app.get("/", function(req, res){
-    
-// });
-
-
-
 app.listen(process.env.PORT, process.env.IP, function(){
     console.log("Server Started");
 });
